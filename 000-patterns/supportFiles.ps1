f86c9ee3f7ec5dac947f368d8da43db493a69352
Marc van Eijk <marcvaneijk@users.noreply.github.com>
2016-10-03 23:39:28 +0200

    Update supportFiles.ps1

--- 000-patterns/supportFiles.ps1
+++ 000-patterns/supportFiles.ps1
@@ -1,6 +1,12 @@
+Param(
+  [string]$myparam
+)
+
 # Variables
 $Uri = 'https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/Deployment/'
 $LocalPath = 'c:\AzureStack_TP2_SupportFiles'
+
+
 
 # Create folder
 New-Item $LocalPath -type directory
@@ -10,3 +16,4 @@ Invoke-WebRequest ($uri + 'BootMenuNoKVM.ps1') -OutFile ($LocalPath + '\BootMenu
 Invoke-WebRequest ($uri + 'PrepareBootFromVHD.ps1') -OutFile ($LocalPath + '\PrepareBootFromVHD.ps1')
 Invoke-WebRequest ($uri + 'Unattend.xml') -OutFile ($LocalPath + '\Unattend.xml')
 Invoke-WebRequest ($uri + 'unattend_NoKVM.xml') -OutFile ($LocalPath + '\unattend_NoKVM.xml')
+Write-Output $myparam
